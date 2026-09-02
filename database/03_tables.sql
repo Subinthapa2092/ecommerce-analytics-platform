09e3b4161a5f0206e35a2bc51225f5de6ecc5714
Subin Thapa <subinthapa2092@gmail.com>
2026-09-02 13:19:38 +0545

    completed the desin of the Categories tables with 2 samples data

--- database/03_tables.sql
+++ database/03_tables.sql
@@ -1,6 +1,7 @@
 use EcommerceAnalytics;
 go
 
+--- Creating the Customers Tables 
 create table dbo.Customers
 (CustomerID int identity(1,1) not null,
 FirstName Nvarchar(100) not null,
@@ -12,3 +13,17 @@ City Nvarchar(100) null,
 Country Nvarchar(50) null,
 constraint PK_Customers Primary key(CustomerID));
 go
+
+--- Creating the Categories Tables 
+use EcommerceAnalytics;
+go
+
+create table dbo.Categories
+(
+    CategoryID int identity(1,1) not null,
+    CategoryName nvarchar(100) not null,
+    ParentCategoryID int null,
+    constraint PK_Categories primary key (CategoryID),
+    constraint FK_Categories_Parent foreign key (ParentCategoryID)
+        references dbo.Categories(CategoryID)
+);
--- database/05_sample_data.sql
+++ database/05_sample_data.sql
@@ -1,4 +1,4 @@
--- inserting the 3 rows with data
+-- inserting the 3 rows for Customers Data
 insert into dbo.Customers(FirstName,LastName,
 Email,Phone,
 DateofBirth,City,Country)values
@@ -6,3 +6,8 @@ DateofBirth,City,Country)values
 ('Rahul','Sharma','rahul123@gmail.com','9761808910','2004-09-20','NewRoad-09,Kathmandu','Nepal'),
 ('Mahin','Rajat','mahin12@gmail.com','9733343343','2009-02-20','NewDelhi','India');
 select * from dbo.Customers;
+-- inserting the 3 rows for the 
+insert into dbo.Categories(CategoryName,ParentCategoryID)
+values('Electronics',null),
+('Phones',1);
+select * from dbo.Categories;
